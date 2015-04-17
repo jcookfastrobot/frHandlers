db449ea2a9986364e6d94b90259f5bf5f081f2ba
John Cook <john_cook@gap.com>
2015-04-17 11:34:18 -0700

    [JC] Updating frHandlers

--- files/default/cookbook_versions.rb
+++ files/default/cookbook_versions.rb
@@ -7,6 +7,12 @@ class Chef
 
       def report
         cookbooks = run_context.cookbook_collection
+#        cookbook_vers = {}
+#        cookbooks.keys.each do |cb|
+#          cookbook_vers[cb.name.to_s] = cb.version.to_s
+#        end
+#        node.default[cookbooks] = cookbook_vers
+
         Chef::Log.info("Cookbooks and versions run: #{cookbooks.keys.map {|x| cookbooks[x].name.to_s + " " + cookbooks[x].version} }")
       end
     end
--- recipes/default.rb
+++ recipes/default.rb
@@ -10,20 +10,20 @@
 include_recipe "ohai"
 include_recipe "chef_handler"
 
-# Log cookbook and version if chef-client successfully converges 
-cookbook_file "#{node["chef_handler"]["handler_path"]}/cookbook_versions.rb" do
-  source "cookbook_versions.rb"
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
-end
+# Log cookbook and version if chef-client successfully converges
+#cookbook_file "#{node["chef_handler"]["handler_path"]}/cookbook_versions.rb" do
+#  source "cookbook_versions.rb"
+#  owner 'root'
+#  group 'root'
+#  mode '0755'
+#  action :create
+#end
 
-chef_handler "Chef::Handler::CookbookVersionsHandler" do
-  source "#{node["chef_handler"]["handler_path"]}/cookbook_versions.rb"
-  supports :report => true
-  action :enable
-end
+#chef_handler "Chef::Handler::CookbookVersionsHandler" do
+#  source "#{node["chef_handler"]["handler_path"]}/cookbook_versions.rb"
+#  supports :report => true
+#  action :enable
+#end
 
 if node["platform_family"] == "rhel"
   ohai 'reload_rpm' do
@@ -37,5 +37,27 @@ if node["platform_family"] == "rhel"
     group 'root'
     mode '0755'
     notifies :reload, 'ohai[reload_rpm]', :immediately
+    not_if { File.exist?("#{node['ohai']['plugin_path']}/rpm.rb") }
   end
 end
+
+ohai 'reload_chef_gems' do
+  plugin 'chef_gems'
+  action :nothing
+end
+
+cookbook_file "#{node['ohai']['plugin_path']}/chef_gems.rb" do
+  source 'plugins/linux/chef_gems.rb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  notifies :reload, 'ohai[reload_chef_gems]', :immediately
+end
+
+cookbooks = run_context.cookbook_collection
+cookbook_vers = {}
+cookbooks.keys.each do |cb|
+  cookbook_vers[cb.to_s] = cookbooks[cb].version.to_s
+end
+puts cookbook_vers
+node.default[:cookbooks] = cookbook_vers
